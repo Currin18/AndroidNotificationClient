6d3ccf9948f5aab6bec3dbd7c3b0f8a1f18b8647
Jesus Moreira <currin18@gmail.com>
2016-02-09 21:27:31 +0100

    Initial Clases

---
+++ AndroidNotificationClient/app/src/main/java/com/currin18/androidnotificationclient/QuickstartPreferences.java
@@ -0,0 +1,9 @@
+package com.currin18.androidnotificationclient;
+
+/**
+ * Created by Jesus on 09/02/2016.
+ */
+public class QuickstartPreferences {
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
